fd04d8dc931510d244a23927bc32f0c10130ad33
Muhammadjon <20mamarasulov@gmail.com>
2021-09-15 09:54:21 +0900

    Initial Commit

---
+++ RemoteRepo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RemoteRepo
+//
+//  Created by Muhammadjon Mamarasulov on 9/15/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RemoteRepo/RemoteRepoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemoteRepoApp.swift
+//  RemoteRepo
+//
+//  Created by Muhammadjon Mamarasulov on 9/15/21.
+//
+
+import SwiftUI
+
+@main
+struct RemoteRepoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
