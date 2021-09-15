54647e8b58c7cd3e61c9fea15683a69291b3ddfc
Muhammadjon <20mamarasulov@gmail.com>
2021-09-15 10:08:43 +0900

    AI added

--- RemoteRepo/ContentView.swift
+++ RemoteRepo/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                 Text("BBC News")
                 Text("CNN News")
                 Text("Newyork News")
+                Text("AI News")
             }
            
                 
