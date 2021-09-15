aed698e0b87aed318d328eed2b1af4b5bcbc678f
Muhammadjon <20mamarasulov@gmail.com>
2021-09-15 10:01:50 +0900

    added NavBar

--- RemoteRepo/ContentView.swift
+++ RemoteRepo/ContentView.swift
@@ -9,8 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            Text("BBC News")
+                .navigationBarTitle("BBC").navigationBarTitleDisplayMode(.inline)
+        }
+            
     }
 }
 
